a54d01aed49cdad221ce6d67f5ab8e1f82a7ef50
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-06-22 11:05:06 +0900

    first import

---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/user"
+	"path/filepath"
+
+	"golang.org/x/oauth2"
+
+	"github.com/google/go-github/v45/github"
+	"gopkg.in/yaml.v2"
+)
+
+type config struct {
+	User       string `yaml:"user"`
+	OAuthToken string `yaml:"oauth_token"`
+}
+
+type response struct {
+	Permission string `json:"permission"`
+	RoleName   string `json:"role_name"`
+	User       struct {
+		AvatarURL        string `json:"avatar_url"`
+		EventsURL        string `json:"events_url"`
+		FollowersURL     string `json:"followers_url"`
+		FollowingURL     string `json:"following_url"`
+		GistsURL         string `json:"gists_url"`
+		GravatarID       string `json:"gravatar_id"`
+		HTMLURL          string `json:"html_url"`
+		ID               int64  `json:"id"`
+		Login            string `json:"login"`
+		NodeID           string `json:"node_id"`
+		OrganizationsURL string `json:"organizations_url"`
+		Permissions      struct {
+			Admin    bool `json:"admin"`
+			Maintain bool `json:"maintain"`
+			Pull     bool `json:"pull"`
+			Push     bool `json:"push"`
+			Triage   bool `json:"triage"`
+		} `json:"permissions"`
+		ReceivedEventsURL string `json:"received_events_url"`
+		ReposURL          string `json:"repos_url"`
+		RoleName          string `json:"role_name"`
+		SiteAdmin         bool   `json:"site_admin"`
+		StarredURL        string `json:"starred_url"`
+		SubscriptionsURL  string `json:"subscriptions_url"`
+		Type              string `json:"type"`
+		URL               string `json:"url"`
+	} `json:"user"`
+}
+
+func main() {
+	u, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, err := ioutil.ReadFile(filepath.Join(u.HomeDir, ".config", "hub"))
+
+	var cfg map[string][]config
+
+	err = yaml.Unmarshal(b, &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gcs, ok := cfg["github.com"]
+	if !ok || len(gcs) == 0 {
+		log.Fatal("GitHub token not found")
+	}
+	gc := gcs[0]
+
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: gc.OAuthToken},
+	)
+	client := github.NewClient(oauth2.NewClient(context.Background(), ts))
+
+	resp, _, err := client.Repositories.GetPermissionLevel(
+		context.Background(),
+		os.Args[1],
+		os.Args[2],
+		gc.User)
+	if err != nil {
+		fmt.Printf("You don't have permission for %s/%s: %v\n", os.Args[1], os.Args[2], err)
+	} else {
+		fmt.Printf("You are %s on %s/%s\n", *resp.Permission, os.Args[1], os.Args[2])
+	}
+}
